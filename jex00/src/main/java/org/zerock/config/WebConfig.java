2025c4f2cc8e6fa371aa309ad6b1f3e5392bd7c5
UD <susan7744850@gmail.com>
2020-03-03 10:52:10 +0900

    Spring Project_JAVA Configuration version

---
+++ jex00/src/main/java/org/zerock/config/WebConfig.java
@@ -0,0 +1,20 @@
+package org.zerock.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+	
+	@Override
+	protected Class<?>[] getRootConfigClasses(){
+		return new Class[] {RootConfig.class};
+	}
+	
+	@Override
+	protected Class<?>[] getServletConfigClasses(){
+		return null;
+	}
+	
+	protected String[] getServletMappings(){
+		return null;
+	}
+}
