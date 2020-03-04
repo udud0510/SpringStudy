c0174fa2104fbd41e8a00364854c55ab1787da53
UD <susan7744850@gmail.com>
2020-03-04 15:18:56 +0900

    @InitBinder sample

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -1,15 +1,20 @@
 package org.zerock.controller;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
+import org.zerock.domain.TodoDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -17,6 +22,12 @@ import lombok.extern.log4j.Log4j;
 @Controller
 @RequestMapping(value = "/sample/*")
 public class SampleController {
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		binder.registerCustomEditor(java.util.Date.class, new CustomDateEditor(dateFormat, false));
+	}
 
 	@RequestMapping(value = "")
 	public void basic() {
@@ -62,5 +73,11 @@ public class SampleController {
 	public String ex02Bean(SampleDTOList list) {
 		log.info("list dtos : " + list);
 		return "ex02Bean";
+	}
+
+	@GetMapping("/ex03")
+	public String ex03(TodoDTO todo) {
+		log.info("todo : " + todo);
+		return "ex03";
 	}
 }
