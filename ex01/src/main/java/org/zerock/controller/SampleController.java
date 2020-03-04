f37012117508da5238c34809d15c315019898511
UD <susan7744850@gmail.com>
2020-03-04 14:35:02 +0900

    @RequestParam Declaration variable

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.zerock.domain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
@@ -32,5 +33,12 @@ public class SampleController {
 	public String ex01(SampleDTO dto) {
 		log.info(""+dto);
 		return "ex01";
+	}
+
+	@GetMapping("/ex02")
+	public String ex02(@RequestParam("name") String name, @RequestParam("age") int age) {
+		log.info("name : "+ name );
+		log.info("age : "+ age );
+		return "ex02";
 	}
 }
