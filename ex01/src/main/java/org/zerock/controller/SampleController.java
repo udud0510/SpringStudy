5be3d80a2d2d18926b1999fe6d76cc17bb43ba6e
UD <susan7744850@gmail.com>
2020-03-04 14:18:13 +0900

    DTO Sample

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.zerock.domain.SampleDTO;
 
 import lombok.extern.log4j.Log4j;
 
@@ -25,5 +26,11 @@ public class SampleController {
 	@GetMapping("/basicOnlyGet")
 	public void basicGet2() {
 		log.info("basic get only get...........");
+	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+		log.info(""+dto);
+		return "ex01";
 	}
 }
--- ex01/src/main/java/org/zerock/domain/SampleDTO.java
+++ ex01/src/main/java/org/zerock/domain/SampleDTO.java
@@ -5,5 +5,5 @@ import lombok.Data;
 @Data
 public class SampleDTO {
 	private String name;
-	private int age;
+	private String age;
 }
