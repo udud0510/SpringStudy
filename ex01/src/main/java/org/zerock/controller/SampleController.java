54d2ffb7c6699cf3b87c8c044bea71511b54f54b
UD <susan7744850@gmail.com>
2020-03-05 11:44:15 +0900

    file upload sample

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.zerock.domain.SampleDTO;
 import org.zerock.domain.SampleDTOList;
 import org.zerock.domain.TodoDTO;
@@ -72,7 +74,7 @@ public class SampleController {
 		log.info("ids : " + Arrays.toString(ids));
 		return "ex02Array";
 	}
-	
+
 	@GetMapping("/ex02Bean")
 	public String ex02Bean(SampleDTOList list) {
 		log.info("list dtos : " + list);
@@ -84,35 +86,52 @@ public class SampleController {
 		log.info("todo : " + todo);
 		return "ex03";
 	}
-	
+
 	@GetMapping("/ex04")
 	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
-		log.info("dto:"+dto);
-		log.info("page:"+page);
-		
+		log.info("dto:" + dto);
+		log.info("page:" + page);
+
 		return "/sample/ex04";
 	}
-	
+
 	@GetMapping("/ex06")
 	public @ResponseBody SampleDTO ex06() {
 		log.info("/ex06...............");
-		
+
 		SampleDTO dto = new SampleDTO();
 		dto.setAge(10);
 		dto.setName("Lee Yu Jin");
-		
+
 		return dto;
 	}
-	
+
 	@GetMapping("/ex07")
 	public ResponseEntity<String> ex07() {
 		log.info("/ex07...............");
-		
+
 		String msg = "{\"name\": \"LeeYuJin\"}";
-		
+
 		HttpHeaders header = new HttpHeaders();
-		header.add("Content-Type","application/json;charset=UTF-8");
+		header.add("Content-Type", "application/json;charset=UTF-8");
 		return new ResponseEntity<>(msg, header, HttpStatus.OK);
+	}
+
+	@GetMapping("/exUpload")
+	public void exUpload() {
+		log.info("/exUpload.....................");
+	}
+
+	@PostMapping("/exUploadPost")
+	public void exUploadPost(ArrayList<MultipartFile> files) {
+
+		files.forEach(file -> {
+			log.info("..........................................");
+			log.info("name : " + file.getOriginalFilename());
+			log.info("size : " + file.getSize());
+
+		});
+
 	}
 
 }
