1295f454a0873f0ded8e3ed08dd8ce6cba23e880
UD <susan7744850@gmail.com>
2020-03-05 10:20:02 +0900

    Use @ModelAttribute

--- ex01/src/main/java/org/zerock/controller/SampleController.java
+++ ex01/src/main/java/org/zerock/controller/SampleController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -78,5 +79,13 @@ public class SampleController {
 	public String ex03(TodoDTO todo) {
 		log.info("todo : " + todo);
 		return "ex03";
+	}
+	
+	@GetMapping("/ex04")
+	public String ex04(SampleDTO dto, @ModelAttribute("page") int page) {
+		log.info("dto:"+dto);
+		log.info("page:"+page);
+		
+		return "/sample/ex04";
 	}
 }
