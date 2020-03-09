e6aa4fe26dce605acb39a48324cbc9c30790f333
UD <susan7744850@gmail.com>
2020-03-09 10:41:48 +0900

    Create SQL Mapper

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,7 +7,7 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 
-	@Select("select * from tbl_board where bno>0")
+	//@Select("select * from tbl_board where bno>0")
 	public List<BoardVO> getList();
 
 }
