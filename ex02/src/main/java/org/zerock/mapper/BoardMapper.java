b69b28ecb389ccd17033e79073544a3814e7e3d7
UD <susan7744850@gmail.com>
2020-03-09 11:47:05 +0900

    make CRUD

--- ex02/src/main/java/org/zerock/mapper/BoardMapper.java
+++ ex02/src/main/java/org/zerock/mapper/BoardMapper.java
@@ -7,7 +7,15 @@ import org.zerock.domain.BoardVO;
 
 public interface BoardMapper {
 
-	//@Select("select * from tbl_board where bno>0")
+	// @Select("select * from tbl_board where bno>0")
 	public List<BoardVO> getList();
+
+	public void insert(BoardVO board);
+
+	public BoardVO read(Long bno);
+
+	public int delete(Long bno);
+
+	public int update(BoardVO board);
 
 }
--- ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
+++ ex02/src/test/java/org/zerock/mapper/BoardMapperTests.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
@@ -16,9 +17,43 @@ public class BoardMapperTests {
 
 	@Setter(onMethod_ = @Autowired)
 	private BoardMapper mapper;
-
+/*
 	@Test
 	public void testGetList() {
 		mapper.getList().forEach(board -> log.info(board));
+	}
+
+	@Test
+	public void testInsert() {
+		BoardVO board = new BoardVO();
+		board.setTitle("새로 작성하는 글");
+		board.setContent("새로 작성하는 내용");
+		board.setWriter("newbie");
+
+		mapper.insert(board);
+		log.info(board);
+	}
+	
+	@Test
+	public void testRead() {
+		BoardVO board = mapper.read(6L);
+		log.info(board);
+	}
+	
+	@Test
+	public void testDelete() {
+		log.info("DELETE COUNT: " + mapper.delete(4L));
+	}
+*/
+	@Test
+	public void testUpdate() {
+		BoardVO board = new BoardVO();
+		board.setBno(5L);
+		board.setTitle("수정된 제목");
+		board.setContent("수정된 내용");
+		board.setWriter("user00");
+		
+		int count=mapper.update(board);
+		log.info("UPDATE COUNT: " + count);
 	}
 }
