6df95f61359767266fd5dde38e79cf38443053fe
UD <susan7744850@gmail.com>
2020-03-10 11:33:24 +0900

    Modify() Remove()

--- ex02/src/main/java/org/zerock/controller/BoardController.java
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -43,4 +43,26 @@ public class BoardController {
 		log.info("/get");
 		model.addAttribute("board", service.get(bno));
 	}
+
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		log.info("modify: " + board);
+
+		if (service.modify(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list/";
+	}
+
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		log.info("remove: " + bno);
+
+		if (service.remove(bno)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+
+		return "redirect:/board/list/";
+	}
 }
--- ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -47,11 +47,26 @@ public class BoardControllerTests {
 				.andReturn().getModelAndView().getViewName();
 		log.info(resultPage);
 	}
-	
+
 	@Test
 	public void testGet() throws Exception {
-		log.info(
-				mockMvc.perform(MockMvcRequestBuilders.get("/board/get").param("bno", "5")).andReturn().getModelAndView().getModelMap());
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/get").param("bno", "5")).andReturn()
+				.getModelAndView().getModelMap());
 	}
 
+	@Test
+	public void testModify() throws Exception {
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify").param("bno", "1").param("title", "수정된 테스트 새글 제목")
+						.param("content", "수정된 테스트 새글 내용").param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+		log.info(resultPage);
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove").param("bno", "8")).andReturn()
+				.getModelAndView().getViewName();
+		log.info(resultPage);
+	}
 }
