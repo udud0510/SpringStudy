b96d84227cc21963e537df0c7345c8b580cb4e79
UD <susan7744850@gmail.com>
2020-03-30 09:54:06 +0900

    Simple Text return

--- ex03/src/main/java/org/zerock/controller/SampleController.java
+++ ex03/src/main/java/org/zerock/controller/SampleController.java
@@ -1,5 +1,19 @@
 package org.zerock.controller;
 
-public class SampleController {
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import lombok.extern.log4j.Log4j;
+
+@RestController
+@RequestMapping("/sample")
+@Log4j
+public class SampleController {
+	@GetMapping(value = "/getText", produces = "text/plain; charset=UTF-8")
+	public String getText() {
+		log.info("MIME TYPE : "+MediaType.TEXT_PLAIN_VALUE);
+		return "æ»≥Á«œººø‰";
+	}
 }
