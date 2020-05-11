c4b2c816c0261c27eb9303f485c416b0e3bdc6a0
UD <susan7744850@gmail.com>
2020-05-11 10:49:28 +0900

    Basic AOP Test

---
+++ ex04/src/test/java/org/zerock/service/SampleServiceTests.java
@@ -0,0 +1,23 @@
+package org.zerock.service;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@Log4j
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
+public class SampleServiceTests {
+    @Setter(onMethod_ = @Autowired)
+    private SampleService service;
+
+    @Test
+    public void testClass(){
+        log.info(service);
+        log.info(service.getClass().getName());
+    }
+}
