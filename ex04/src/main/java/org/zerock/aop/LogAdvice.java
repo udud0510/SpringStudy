d1a623f8e12daf2975b805810c99bd13ce5113b8
UD <susan7744850@gmail.com>
2020-05-11 11:36:47 +0900

    @Around와 ProceedingJoinPoint

--- ex04/src/main/java/org/zerock/aop/LogAdvice.java
+++ ex04/src/main/java/org/zerock/aop/LogAdvice.java
@@ -1,10 +1,14 @@
 package org.zerock.aop;
 
 import lombok.extern.log4j.Log4j;
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Log4j
@@ -20,10 +24,30 @@ public class LogAdvice {
         log.info("str1 : " + str1);
         log.info("str2 : " + str2);
     }
-
+    // 일부러 에러 발생시키고 동작 확인
     @AfterThrowing(pointcut = "execution(* org.zerock.service.SampleService*.*(..))", throwing = "exception")
     public void logException(Exception exception) {
         log.info("Exception....!!!!");
         log.info("exception: " + exception);
+    }
+
+    @Around("execution(* org.zerock.service.SampleService*.*(..))")
+    public Object logTime(ProceedingJoinPoint pjp) {
+        long start = System.currentTimeMillis();
+        log.info("Target: " + pjp.getTarget());
+        log.info("Param: " + Arrays.toString(pjp.getArgs()));
+
+        // invoke method
+        Object result = null;
+
+        try {
+            result = pjp.proceed();
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        long end = System.currentTimeMillis();
+
+        log.info("TIME: " + (end - start));
+        return result;
     }
 }
