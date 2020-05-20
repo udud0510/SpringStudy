a76be3625a0b43739bfba8e35c45ea3aa0973e7c
UD <susan7744850@gmail.com>
2020-05-20 10:59:53 +0900

    Upload File - transferTo()

--- ex05/src/main/java/org/zerock/controller/UploadController.java
+++ ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileReader;
+
 @Controller
 @Log4j
 public class UploadController {
@@ -18,10 +21,20 @@ public class UploadController {
 
     @PostMapping("/uploadFormAction")
     public void uploadFormPost(MultipartFile[] uploadFile, Model model) {
+
         for (MultipartFile multipartFile : uploadFile) {
             log.info("----------------------------");
             log.info("Upload File Name : " + multipartFile.getOriginalFilename());
             log.info("Upload File Size : " + multipartFile.getSize());
+
+            File saveFile = new File(multipartFile.getOriginalFilename());
+
+            try {
+                multipartFile.transferTo(saveFile);
+                log.info("save");
+            } catch (Exception e) {
+                log.error(e.getMessage());
+            }
         }
     }
 }
